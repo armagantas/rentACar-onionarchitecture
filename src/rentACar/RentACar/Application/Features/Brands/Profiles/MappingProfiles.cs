ab3799934d96b759ad3c0ef741a60c5d4e11f7a9
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-02-02 00:13:07 +0300

    Page filtering and query implementation done.

--- src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,11 +1,9 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Profiles
 {
@@ -15,6 +13,9 @@ namespace Application.Features.Brands.Profiles
         {
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
             CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+            CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+            CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
+
         }
     }
 }
