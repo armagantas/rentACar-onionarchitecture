d41d13ab609b36c3ae80404d5315c4d0a8bed4cc
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-03-13 02:30:11 +0300

    Work rules and SubClass Type IoC Implementation done.

--- src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,5 @@
 ﻿
+using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
@@ -19,14 +20,18 @@ namespace Application.Features.Brands.Commands.Create
         {
             private readonly IBrandRepository _brandRepository;
             private readonly IMapper _mapper;
+            private readonly BrandBusinessRules _brandBusinessRules;
 
-            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
             {
                 _brandRepository = brandRepository;
                 _mapper = mapper;
+                _brandBusinessRules = brandBusinessRules;
             }
             public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
             {
+                await _brandBusinessRules.IsBrandNameUnique(request.Name);
+
                 Brand brand = _mapper.Map<Brand>(request);
                 brand.Id = Guid.NewGuid();
 
