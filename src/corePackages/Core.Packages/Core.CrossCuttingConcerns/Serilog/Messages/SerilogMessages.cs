611924fbb819a7128294179e78e90940ebb7f427
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-03-18 20:21:07 +0300

    Log implementation done.

---
+++ src/corePackages/Core.Packages/Core.CrossCuttingConcerns/Serilog/Messages/SerilogMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Messages
+{
+    public static class SerilogMessages
+    {
+        public static string NullOptionsMessage = "You have sent a null options object. Please check your configuration.";
+    }
+}
